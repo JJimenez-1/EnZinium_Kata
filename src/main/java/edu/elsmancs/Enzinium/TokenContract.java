e9f421f824fe1bb7ca1ef037d11cf476cf230e04
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-02-16 16:30:35 +0100

    feat(Address): Añadir propiedades a la clase Address con sus getters y setters

--- src/main/java/edu/elsmancs/Enzinium/TokenContract.java
+++ src/main/java/edu/elsmancs/Enzinium/TokenContract.java
@@ -1,5 +1,33 @@
 package edu.elsmancs.Enzinium;
 
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TokenContract {
+    private String name;
+    private String symbol;
+    private double totalSupply;
+    private final Address owner;
+    private Map<PublicKey, Double> balances = new HashMap<PublicKey, Double>();
+
+    public TokenContract(Address owner) {
+        this.owner = owner;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String symbol() {
+        return symbol;
+    }
+
+    public double totalSupply() {
+        return totalSupply;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
